b78e04d5166892d62cce53f77de3e0e5bf1b6312
nonl4331 <nicholasl0003@gmail.com>
2026-07-09 11:39:46 +1000

    added more materials + basic scene

--- src/main.rs
+++ src/main.rs
@@ -14,39 +14,86 @@ mod prelude {
 
 use prelude::*;
 
-fn main() {
-    let mats = vec![Material::Lambertian(Lambertian::new(Vec3::splat(0.5)))];
-    let obj = vec![
-        Sphere::new(-Vec3::Z, 0.5, 0),
-        Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, 0),
-    ];
-    let world = World::new(obj, mats);
+const WIDTH: usize = 1200;
+const HEIGHT: usize = 675;
+const SAMPLES: usize = 10;
 
-    let cam = Cam::new(
-        Vec3::ZERO,
-        -Vec3::Z,
-        Vec3::Y,
-        90.0,
-        1.0,
-        1024,
-        1024,
-    );
+fn main() {
+    let mut mats = vec![
+        Material::Lambertian(Lambertian::new(Vec3::splat(0.5))),
+        Material::Dielectric(Dielectric::new(1.5)),
+    ];
+    let mut obj = vec![Sphere::new(-Vec3::Y * 1000.0, 1000.0, 0)];
 
     let mut rng = rand::rng();
 
-    let mut img = vec![Vec3::ZERO; 1024 * 1024];
+    for a in -11..11 {
+        for b in -11..11 {
+            let centre = Vec3::new(
+                a as f32 + 0.9 * rng.random::<f32>(),
+                0.2,
+                b as f32 + 0.9 * rng.random::<f32>(),
+            );
+            if (centre - Vec3::new(4.0, 0.2, 0.0)).mag() > 0.9 {
+                let p = rng.random::<f32>();
+                if p < 0.8 {
+                    let c1 = Vec3::new(rng.random(), rng.random(), rng.random());
+                    let c2 = Vec3::new(rng.random(), rng.random(), rng.random());
+                    mats.push(Material::Lambertian(Lambertian::new(c1 * c2)));
+                    obj.push(Sphere::new(centre, 0.2, mats.len() - 1));
+                } else if p < 0.95 {
+                    let colour = Vec3::new(rng.random(), rng.random(), rng.random());
+                    mats.push(Material::Metal(Metal::new(colour, rng.random())));
+                    obj.push(Sphere::new(centre, 0.2, mats.len() - 1));
+                } else {
+                    obj.push(Sphere::new(centre, 0.2, 1));
+                }
+            }
+        }
+    }
+    obj.push(Sphere::new(Vec3::Y, 1.0, 1));
+
+    mats.push(Material::Lambertian(Lambertian::new(Vec3::new(
+        0.4, 0.2, 0.1,
+    ))));
+    obj.push(Sphere::new(Vec3::new(-4.0, 1.0, 0.0), 1.0, mats.len() - 1));
+
+    mats.push(Material::Metal(Metal::new(Vec3::new(0.7, 0.6, 0.5), 0.0)));
+    obj.push(Sphere::new(Vec3::new(4.0, 1.0, 0.0), 1.0, mats.len() - 1));
+
+    let world = World::new(obj, mats);
+
+    let cam = Cam::new(
+        Vec3::new(13.0, 2.0, 3.0),
+        Vec3::ZERO,
+        Vec3::Y,
+        20.0,
+        1.0,
+        WIDTH,
+        HEIGHT,
+    );
+
+    let mut img = vec![Vec3::ZERO; WIDTH * HEIGHT];
     let mut rays = 0;
+    let start = std::time::Instant::now();
     for (i, px) in img.iter_mut().enumerate() {
-        //let x = i % 1024;
-        //let y = i / 1024;
-        //*px = Vec3::new(x as f32 / 1024.0, y as f32 / 1024.0, 1.0);
-        let ray = cam.get_ray(i as u64, &mut rng);
-        let (col, depth) = integrator::Naive::rgb(ray, &world, &mut rng);
-        rays += depth;
+        let mut col = Vec3::zero();
+        for _ in 0..SAMPLES {
+            let ray = cam.get_ray(i as u64, &mut rng);
+            let (ncol, depth) = integrator::Naive::rgb(ray, &world, &mut rng);
+            col += ncol;
+            rays += depth;
+        }
+        col /= SAMPLES as f32;
         *px = col
     }
-    println!("rays: {rays}");
-    save_image("test.png", &img, 1024);
+    let render_time = start.elapsed();
+    println!("took {} seconds", render_time.as_secs_f64());
+    println!(
+        "{rays} rays @ {:.2} MRay/s",
+        (1e-6 * rays as f64) / render_time.as_secs_f64()
+    );
+    save_image("test.png", &img, WIDTH);
 }
 
 fn save_image(name: &str, img: &[Vec3], width: usize) {
--- src/mat.rs
+++ src/mat.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
 
-const EP: f32 = 0.00001;
+const EP: f32 = 0.0001;
 
 pub enum Material {
     Lambertian(Lambertian),
@@ -11,16 +11,16 @@ pub enum Material {
 impl Material {
     pub fn scatter(&self, ray: &mut Ray, sect: &Intersection, rng: &mut impl Rng) {
         match self {
-            Self::Lambertian(m) => Lambertian::scatter(ray, sect, rng),
-            Self::Metal(_) => todo!(),
-            Self::Dielectric(_) => todo!(),
+            Self::Lambertian(_) => Lambertian::scatter(ray, sect, rng),
+            Self::Metal(m) => m.scatter(ray, sect, rng),
+            Self::Dielectric(m) => m.scatter(ray, sect, rng),
         }
     }
     pub fn eval(&self, _wo: Vec3, _wi: Vec3) -> Vec3 {
         match self {
             Self::Lambertian(m) => m.albedo,
-            Self::Metal(_) => todo!(),
-            Self::Dielectric(_) => todo!(),
+            Self::Metal(m) => m.colour,
+            Self::Dielectric(_) => Vec3::ONE,
         }
     }
 }
@@ -39,12 +39,59 @@ impl Lambertian {
 }
 
 pub struct Metal {
-    colour: Vec3,
+    pub colour: Vec3,
     fuzz: f32,
+}
+
+impl Metal {
+    pub fn new(colour: Vec3, fuzz: f32) -> Self {
+        Self {
+            colour,
+            fuzz: fuzz.min(1.0),
+        }
+    }
+    pub fn scatter(&self, ray: &mut Ray, sect: &Intersection, rng: &mut impl Rng) {
+        let wo = -ray.dir;
+
+        let wi = wo.reflected(sect.nor) + (self.fuzz * random_unit_vec(rng));
+        *ray = Ray::new(sect.pos, wi);
+    }
 }
 
 pub struct Dielectric {
     ior: f32,
+}
+
+impl Dielectric {
+    pub fn new(ior: f32) -> Self {
+        Self { ior }
+    }
+    pub fn scatter(&self, ray: &mut Ray, sect: &Intersection, rng: &mut impl Rng) {
+        let wo = -ray.dir;
+
+        let mut eta1 = 1.0;
+        let mut eta2 = self.ior;
+
+        if !sect.out {
+            std::mem::swap(&mut eta1, &mut eta2);
+        }
+        let eta = eta1 / eta2;
+
+        let cosi = wo.dot(sect.nor);
+
+        let r = fresnel_dielectric(eta1, eta2, sect.nor, wo);
+
+        if r >= rng.random() {
+            let wi = wo.reflected(sect.nor);
+            *ray = Ray::new(sect.pos, wi);
+            return;
+        }
+
+        let perp = eta * (cosi * sect.nor - wo);
+        let para = -(1.0 - perp.mag_sq()).abs().sqrt() * sect.nor;
+        let wi = perp + para;
+        *ray = Ray::new(sect.pos, wi);
+    }
 }
 
 fn random_unit_vec(rng: &mut impl Rng) -> Vec3 {
@@ -59,4 +106,24 @@ fn random_unit_vec(rng: &mut impl Rng) -> Vec3 {
             return p * (1.0 / mag.sqrt());
         }
     }
+}
+
+// eta1 = outer ior, eta2 = inner ior
+pub fn fresnel_dielectric(eta1: f32, eta2: f32, nor: Vec3, wo: Vec3) -> f32 {
+    let eta = eta1 / eta2;
+
+    let cosi = wo.dot(nor);
+
+    let sint_sq = eta.powi(2) * (1.0 - cosi.powi(2));
+    let is_tir = sint_sq >= 1.0;
+    if is_tir {
+        return 1.0;
+    }
+
+    let cost = (1.0 - sint_sq).sqrt();
+
+    let rs = ((eta1 * cosi - eta2 * cost) / (eta1 * cosi + eta2 * cost)).powi(2);
+    let rp = ((eta1 * cost - eta2 * cosi) / (eta1 * cost + eta2 * cosi)).powi(2);
+
+    0.5 * (rs + rp)
 }
